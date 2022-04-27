8360ac510b93051b51552e9b9d1c29a5d798ce72
lfmgs1-iscte <100279643+lfmgs1-iscte@users.noreply.github.com>
2022-04-27 23:00:58 +0100

    Update Size2D.java

--- jfreechart-master/src/main/java/org/jfree/chart/block/Size2D.java
+++ jfreechart-master/src/main/java/org/jfree/chart/block/Size2D.java
@@ -174,39 +174,53 @@ public class Size2D implements Cloneable, PublicCloneable, Serializable {
 	 * @param vAlign   the vertical alignment ( {@code  null}  not permitted).
 	 * @return  A rectangle.
 	 */
-	public Rectangle2D createAlignedRectangle2D(Rectangle2D frame, HorizontalAlignment hAlign,
-			VerticalAlignment vAlign) {
-		Args.nullNotPermitted(hAlign, "hAlign");
-		Args.nullNotPermitted(vAlign, "vAlign");
-		double x = Double.NaN;
+	private Rectangle2D createAlignedRectangle2D(Size2D dimensions,
+            Rectangle2D frame, HorizontalAlignment hAlign,
+            VerticalAlignment vAlign) {
+        Args.nullNotPermitted(hAlign, "hAlign");
+        Args.nullNotPermitted(vAlign, "vAlign");
+        double x = x_creation(dimensions, frame, hAlign);
+		double y = y_creation(dimensions, frame, hAlign, vAlign);
+		return new Rectangle2D.Double(x, y, dimensions.width,
+                dimensions.height);
+    	}
+
+	private double y_creation(Size2D dimensions, Rectangle2D frame, HorizontalAlignment hAlign,
+			VerticalAlignment vAlign) throws IllegalStateException {
 		double y = Double.NaN;
-		switch (hAlign) {
-		case LEFT:
-			x = frame.getX();
-			break;
-		case CENTER:
-			x = frame.getCenterX() - (this.width / 2.0);
-			break;
-		case RIGHT:
-			x = frame.getMaxX() - this.width;
-			break;
-		default:
-			throw new IllegalStateException("Unexpected enum value " + hAlign);
-		}
 		switch (vAlign) {
 		case TOP:
 			y = frame.getY();
 			break;
 		case CENTER:
-			y = frame.getCenterY() - (this.height / 2.0);
+			y = frame.getCenterY() - (dimensions.height / 2.0);
 			break;
 		case BOTTOM:
-			y = frame.getMaxY() - this.height;
+			y = frame.getMaxY() - dimensions.height;
 			break;
 		default:
 			throw new IllegalStateException("Unexpected enum value " + hAlign);
 		}
-		return new Rectangle2D.Double(x, y, this.width, this.height);
+		return y;
+	}
+
+	private double x_creation(Size2D dimensions, Rectangle2D frame, HorizontalAlignment hAlign)
+			throws IllegalStateException {
+		double x = Double.NaN;
+		switch (hAlign) {
+		case LEFT:
+			x = frame.getX();
+			break;
+		case CENTER:
+			x = frame.getCenterX() - (dimensions.width / 2.0);
+			break;
+		case RIGHT:
+			x = frame.getMaxX() - dimensions.width;
+			break;
+		default:
+			throw new IllegalStateException("Unexpected enum value " + hAlign);
+		}
+		return x;
 	}
     
 }
